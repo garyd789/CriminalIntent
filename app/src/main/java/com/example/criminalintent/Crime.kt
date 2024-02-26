d516c524e41569ae8c426e7c612d475b258f8bec
Gary Dong <garyd89@bu.edu>
2024-02-25 21:56:07 -0500

    first commit

---
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -0,0 +1,11 @@
+package com.example.criminalintent
+
+import java.util.UUID
+import java.util.Date
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
